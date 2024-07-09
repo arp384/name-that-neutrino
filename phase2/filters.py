55f48eae13191d34c488e15dd4d7f8d3953ad9f0
Andrew Phillips <aphillips@cobalt03.icecube.wisc.edu>
2024-07-09 11:09:39 -0500

    updating uniform_energy_events function

--- phase2/filters.py
+++ phase2/filters.py
@@ -26,7 +26,33 @@ Purpose: Collection of filters for processing of raw simulated icecube data.
 - Filter 3 Filters out non-DAQ frames.
 '''
 
+#gcd file location
 gcd = '/home/aphillips/ntn/GeoCalibDetectorStatus_2012.56063_V1.i3.gz' #should probably make this an input (cmd line arg?) (AP)
+
+#label dictionary (mapping from truth values, to english)
+label_dict = {0:'unclassified',
+                        1:'throughgoing_track',
+                        2:'starting_track',
+                        3:'stopping_track',
+                        4:'skimming_track',
+                        5:'contained_track',
+                        6:'contained_em_hadr_cascade',
+                        7:'contained_hadron_cascade',
+                        8:'uncontained_cascade',
+                        9:'glashow_starting_track',
+                        10:'glashow_electron',
+                        11:'glashow_tau_double_bang',
+                        12:'glashow_tau_lollipop',
+                        13:'glashow_hadronic',
+                        14:'throughgoing_tau',
+                        15:'skimming_tau',
+                        16:'double_bang',
+                        17:'lollipop',
+                        18:'inverted_lollipop',
+                        19:'throughgoing_bundle',
+                        20:'throughgoing_bundle',
+                        21:'tau_to_mu'}
+
 
 """
 Filter 1:
@@ -68,11 +94,13 @@ Filter 2:
 - Takes a random seed (will want to program this to be optional later on.)
 - Is standard to use bin num = 25 to follow most IceCube energy histograms
 """
+
+
+#AP: editing out histogram stuff because unnecessary
 def uniformenergy_events(hdf,bin_num, size,random_seed,subrun = False):
     #open hdf of desired i3 file
     hdf = f'{hdf}'
     hdf_file = h5py.File(hdf, "r+")
-    print(f"Number of Bins: {bin_num}\nNumber of Events per Bin: {size}\nTotal Number of Events: {size*bin_num}") #Get some quick info
 
     #turn hdf into pandas dataframe.  
 
@@ -80,53 +108,24 @@ def uniformenergy_events(hdf,bin_num, size,random_seed,subrun = False):
     run_id = hdf_file['I3EventHeader']['Run'][:] #Run ID
 
     #classifier predictions
-    pred_skim_val = hdf_file['ml_suite_classification']['prediction_0000'][:] #Skimming
-    pred_cascade_val = hdf_file['ml_suite_classification']['prediction_0001'][:] #Cascade
-    pred_tgtrack_val = hdf_file['ml_suite_classification']['prediction_0002'][:] #Through-Going Track
-    pred_starttrack_val = hdf_file['ml_suite_classification']['prediction_0003'][:] #Starting Track
-    pred_stoptrack_val = hdf_file['ml_suite_classification']['prediction_0004'][:] #Stopping Track
+    pred_skim_val = hdf_file['ml_suite_classification']['prediction_0000'][:] #dnn skim prediction
+    pred_cascade_val = hdf_file['ml_suite_classification']['prediction_0001'][:] #dnn cascade prediction
+    pred_tgtrack_val = hdf_file['ml_suite_classification']['prediction_0002'][:] #dnn tg track prediction
+    pred_starttrack_val = hdf_file['ml_suite_classification']['prediction_0003'][:] #dnn start track prediction
+    pred_stoptrack_val = hdf_file['ml_suite_classification']['prediction_0004'][:] #dnn stop track prediction
+    truth_label = hdf_file['truth_classification']['value'][:] #label from mc_labeler (as a number)
+    energy_val = hdf_file['NuGPrimary']['energy'][:] #energy
+    zenith_val = hdf_file['NuGPrimary']['zenith'][:] #zenith
+    ow = hdf_file['I3MCWeightDict']['OneWeight'][:] #oneweight
 
-    truth_label = hdf_file['truth_classification']['value'][:] #mc_labeler value
-
-    energy_val = hdf_file['NuGPrimary']['energy'][:]
-    zenith_val = hdf_file['NuGPrimary']['zenith'][:]
-    
-    ow = hdf_file['I3MCWeightDict']['OneWeight'][:]
-
-    #Dataframe time
+    #turn the above arrays into a pandas dataframe
     df = pd.DataFrame(dict(run = run_id, event = event_id, truth_classification = truth_label, pred_skim = pred_skim_val, pred_cascade = pred_cascade_val,\
         pred_tgtrack = pred_tgtrack_val, pred_starttrack = pred_starttrack_val, pred_stoptrack = pred_stoptrack_val,energy = energy_val, zenith = zenith_val,\
         oneweight = ow))
     
-    hdf_file.close() #close hdf file now that dataframe is made. 
+    hdf_file.close() #close hdf file
     
-    #get truth id label (i.e. as words) into dataframe
-    
-    #Dictionary of event truth labels according to mc_labeler and enums, not DNNClassifier
-    label_dict = {0:'unclassified',
-                        1:'throughgoing_track',
-                        2:'starting_track',
-                        3:'stopping_track',
-                        4:'skimming_track',
-                        5:'contained_track',
-                        6:'contained_em_hadr_cascade',
-                        7:'contained_hadron_cascade',
-                        8:'uncontained_cascade',
-                        9:'glashow_starting_track',
-                        10:'glashow_electron',
-                        11:'glashow_tau_double_bang',
-                        12:'glashow_tau_lollipop',
-                        13:'glashow_hadronic',
-                        14:'throughgoing_tau',
-                        15:'skimming_tau',
-                        16:'double_bang',
-                        17:'lollipop',
-                        18:'inverted_lollipop',
-                        19:'throughgoing_bundle',
-                        20:'throughgoing_bundle',
-                        21:'tau_to_mu'}
-    
-    #turn numbers into words!
+    #make array of english truth classification labels
     word_truth_labels = []
     for x in df['truth_classification']:
         word_truth_labels.append(label_dict[int(x)])
@@ -135,35 +134,31 @@ def uniformenergy_events(hdf,bin_num, size,random_seed,subrun = False):
 
     #add in other key-value pairs into dataframe
 
-    #returns value of max ML score. 
+    #maximum category score returned by DNN
     df['max_score_val'] = df[['pred_skim','pred_cascade','pred_tgtrack','pred_starttrack','pred_stoptrack']].max(axis='columns')
 
-    #Getting largest ML prediction score. 
-
-    #returns index of max ML score. 
+    #based on max score, get dnn category
     df['idx_max_score'] = df[['pred_skim','pred_cascade','pred_tgtrack','pred_starttrack','pred_stoptrack']].idxmax(axis='columns')
     
-
     #Pick events "manually", cut at PeV-ish.
-    
-    truth_vals_list = [1, 2, 3, 4, 6, 7, 8]
+    truth_vals_list = [1, 2, 3, 4, 6, 7, 8] #these are the truth values we want to keep, corresponding to stop,start,tg,casc, and skim
 
+    #take only rows with truth vals in above list
     df_masked = df[df['truth_classification'].isin(truth_vals_list)]
 
+    #remove events with energies > PEV
     df_filtered = df_masked.loc[(np.log10(df_masked['energy'][:]) <= 6)]
-    print("Initial Number of Events:",len(df))
-    print("Number of Events after filtering:",len(df_filtered))
+    
     hist_df, bin_edges_df = np.histogram(np.log10(df_filtered['energy'][:]), density=False,bins=bin_num)
     #Use np.digitize to return index of the bin each energy value belongs to. 
     energy_bin_index = np.digitize(np.log10(df_filtered['energy']),bin_edges_df)
     #take any value less than fist bin is put in bin 1, and any value greater than 25th bin now in bin 25. 
     energy_bin_index = np.clip(energy_bin_index,1,bin_num) 
-    
     #get bins and labels. 
     bins = bin_edges_df
     labels = np.arange(1,bin_num+1)
-
-    df_filtered['binned_log10E'] = pd.cut(x = np.log10(df_filtered['energy']), bins = bins, labels = labels, include_lowest = True) #might throw an error?
+    #AP: I don't know what this is for, might be able to throw out.
+    df_filtered['binned_log10E'] = pd.cut(x = np.log10(df_filtered['energy']), bins = bins, labels = labels, include_lowest = True) 
     #Loop through each bin number and mask out any events not in that bin number.
 
     seed=random_seed
@@ -238,7 +233,7 @@ Filter 3:
 - Maybe make another hdf just in case?
 """
 
-#AP edit: filter3 extracts all the DAQ frames, but keeps them all in a singular I3 instead of splitting up (maybe restore that later?)
+#extracts daq frames, splits i3s into 2 mb files to ease steamhovel processing
 def filter3(infile, run_id,ddir):
     drive, ipath =os.path.splitdrive(infile)
     path, ifn = os.path.split(ipath)
