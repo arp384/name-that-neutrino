f5decfe164aade3e3ef351efbea0260711f54b8b
Andrew Phillips <aphillips@cobalt08.icecube.wisc.edu>
2024-08-09 12:48:40 -0500

    comit so i can change branch

--- phase2/process_files.py
+++ phase2/process_files.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     numu_subruns = pd.read_csv('/home/aphillips/name-that-neutrino/phase2/numu_files.csv')['subrun']
     nue_subruns = pd.read_csv('/home/aphillips/name-that-neutrino/phase2/nue_files.csv')['subrun']
     
-    print('Processing NuE files...')
+   ''' print('Processing NuE files...')
     for subrun in nue_subruns:
         subrun = f'{subrun}'.rjust(6, '0')
         filename = f'classifier_rehyd_DST_IC86.2020_NuE.022067.{subrun}.i3.zst'
@@ -41,7 +41,7 @@ if __name__ == '__main__':
         make_csv(os.path.join(OUTDIR,f'mc_labeled_{filename}.hd5'), OUTDIR, subrun) #makes dataframe of desired events
         do_cuts(os.path.join(OUTDIR,f'mc_labeled_{filename}'), OUTDIR, os.path.join(OUTDIR, f'events_df_{subrun}.csv')) #cut on desired frames
         extract_daq(os.path.join(OUTDIR, f'cuts_mc_labeled_{filename}'), f'22067{subrun}', OUTDIR) #extract daq only and split into 2 mb sizes
-    
+   ''' 
     print('Processing NuMu files...')
     for subrun in numu_subruns:
         subrun = f'{subrun}'.rjust(6, '0')
